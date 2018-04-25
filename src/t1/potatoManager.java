e34ef604fd39f803a7dc579ccd5d3a74ad772f54
Francisca-PC\Francisca <iwakika@gmail.com>
2018-04-25 08:35:28 -0300

    potatoManeger

--- src/t1/potatoManager.java
+++ src/t1/potatoManager.java
@@ -1,20 +1,27 @@
 package t1;
 
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+
+
 public class potatoManager {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}//
+	}
 	
 	
-	/**Parametros usados na calibragem do robo
-	**/
-	private static int grau_90_velocidade_100 = 1900; 
-	private static int distancia_10_velocidade_100= 2300; //10 cm
-	private static int vel_calibragem_100 = 100;
-	private static int valor_distancia = 25;
+	
+	/**
+	 * Parametros de calibragem baseados na rotacao do motor
+	 * @author fexavier
+	 *
+	 */
 	private static final int VELOCIDADE = 320;
+	private static int grau_90_MotorRotate_100  = 200;
+	private static int distancia_10_MotorRotate_100 = 200; 
+	private static int rotate_Calibragem_100 = 100;	
 	
 	public static enum Direcao {
 	       DIREITA,ESQUERDA,FRENTE,TRAZ;
@@ -22,37 +29,121 @@ public class potatoManager {
 	
 	
 	
-	public static void calibragem() throws InterruptedException {
-		Thread.sleep (3000);
+	public static void calibragemTeste() throws InterruptedException {
+		/*Thread.sleep (3000);
 		vira(grauX(90), Direcao.DIREITA);
 		Thread.sleep (3000);
 		vira(grauX(90), Direcao.DIREITA);
 		Thread.sleep (3000);
 		vira(grauX(90), Direcao.DIREITA);
 		Thread.sleep (3000);
-		andar(distanciaX(20));
+		andar(distanciaX(20));*/
 	}
 	
+	public static void  menuCalibragemDistanca(){
+	Boolean sair = false;
+	System.out.println("Distancia_10");
+	System.out.println("Atuaal:"+ distancia_10_MotorRotate_100 );
+	
+		while(sair){
+			
+			
+			int buttonPress =Button.readButtons();
+			
+			switch (buttonPress) {
+			
+			case Button.ID_RIGHT:
+				
+				distancia_10_MotorRotate_100  += 10;
+				
+			break;
+			case Button.ID_LEFT:
+
+				distancia_10_MotorRotate_100  -= 10;
+				
+			break;
+					
+			case Button.ID_ENTER:
+
+				sair = true;
+				
+			break;
+				
+
+			default:
+				break;
+			}
+			
+			
+			
+		}
+		
+		
+	}
+	
+	public static void  menuCalibragemGrau(){
+		Boolean sair = false;
+		System.out.println("Grau_90");
+		System.out.println("Atual:"+ grau_90_MotorRotate_100 );
+		
+			while(sair){
+				
+				
+				int buttonPress =Button.readButtons();
+				
+				switch (buttonPress) {
+				
+				case Button.ID_RIGHT:
+					
+					grau_90_MotorRotate_100  += 10;
+					
+				break;
+				case Button.ID_LEFT:
+
+					grau_90_MotorRotate_100  -= 10;
+					
+				break;
+						
+				case Button.ID_ENTER:
+
+					sair = true;
+					
+				break;
+					
+
+				default:
+					break;
+				}
+				
+				
+				
+			}
+			
+			
+		}
+	
+	
+	
 	public void defineVelocidade(int velocidade) {
-		//Motor.B.setSpeed(VELOCIDADE);
-		//Motor.C.setSpeed(VELOCIDADE);
+		Motor.B.setSpeed(VELOCIDADE);
+		Motor.C.setSpeed(VELOCIDADE);
 		
 	}
 	
 	/** A partir de um ponto parado o Bot vira contadorX para a direita se true,
 	ou contadorx para esquerda se direita  for false
 	*/
-	public static void vira (int contador, Direcao direcao) {
+	public static void vira (int motorRotate, Direcao direcao) {
 			
 		switch (direcao) {
 			case DIREITA:
-			rotate(contador); //Motor.B.rotate(contador);
-			rotate(-contador);//Motor.C.rotate(-contador);
+			Motor.B.rotate(motorRotate);
+			Motor.C.rotate(-motorRotate);
 			break;
 			
 			case ESQUERDA:
-				rotate(-contador);//Motor.B.rotate(-contador);
-				rotate(contador);//Motor.C.rotate(contador);
+				Motor.B.rotate(-motorRotate);
+				Motor.C.rotate(motorRotate);
 			break;	
 
 		default:
@@ -63,79 +154,38 @@ public class potatoManager {
 	}
 	
 
-	public static void andar(int contador) {		
-			rotate(contador); //Motor.B.rotate(contador);
-			rotate(contador); //Motor.C.rotate(contador);					
+	public static void andar(int motorRotate) {		
+			 Motor.B.rotate(motorRotate);
+			 Motor.C.rotate(motorRotate);					
 		
 	}
-	/**
-	Retorna o contador para  girar o grau em x em qualquer velocidade,
-	grau_90_velocidade_100 deve ter sido previmente calibrada na velocidade base 100
-	**/
+
 	
-	public static int grauX(int grau) {
-		//divide pela a velocidade de teste 100
-		int graux =  (vel_calibragem_100*grau_vel_100(grau))/VELOCIDADE ;
+	/**Tranforma o valor do angulo em valores de rotacoes do motor
+	 * 
+	 * @param grau quantidade de graus que deseja obter o valor em rotacoes do motor
+	 * @return
+	 */
+	
+	
+	public static int grausToMotorRotate(int grau){
+		int motorRotate = (grau_90_MotorRotate_100 * grau) /90;
 				
-		return graux;
+		return  motorRotate;
 	}
 	
+	/**Tranforma o valor do angulo em valores de rotacoes do motor
+	 * 
+	 * @param grau quantidade de graus que deseja obter o valor em rotacoes do motor
+	 * @return
+	 */
+	
+	public static int dintanceToMotorRotate(int distance){
+	
+		int motorRotate = (distancia_10_MotorRotate_100 * distance ) / 10;
+		return motorRotate ;
+	}
 
-	/**
-	Retorna o contador para  a distncia X (em cm) em qualquer velocidade,
-	distancia_10_velocidade_100 deve ter sido previmente calibrada na velocidade base 100
-	
-	**/
-	public static int distanciaX(int distancia) {
-		//divide pela a velocidade de teste 100
-		int distanciax =  (vel_calibragem_100* Distancia_vel_100(distancia))/VELOCIDADE ;
-		
-		return distanciax;
-		
 
-	} 	
-	
-	
-	/**Retorna o valor do "contador" baseado no grau informado por parametro
-	usando a velocidade "100".
-	o valor "grau_90_velocidade_100"
-	deve ser previamente calibrada para o Bot, setando o quanto  necessrio para virar 90 na velocidade 100
-	**/
-	private static int grau_vel_100 (int grau) {
-		
-		//grau_90 = 100; // varivel para alterar				
-		int contador = 0;		
-		contador = (grau_90_velocidade_100 * grau)/90;	
-		
-		return contador;
-		
-	}
-	
-	/**Retorna o Valor do "Contador" baseado da distancia informado por parmetro
-	"O valor deve distancia_10_velocidade_100" deve ser previamente calibrado para o Bot, setando o quanto  necessrio para
-	o  Bot andar 10 cm na velocidade base 100
-	**/
-	private static int Distancia_vel_100(int distancia) {
-	    //distancia_10 = 100;
-		int contador = 0;
-		contador =  (distancia_10_velocidade_100 * distancia)/10;
-		
-		return contador;
-		
-	}
-	
-	
-public static void rotate (int i) {
-		
-	}
-	
-	
-	public static  void forward(){
-		
-	}
-	
-	public static void backward() {
-		
-	}	
 
 }
